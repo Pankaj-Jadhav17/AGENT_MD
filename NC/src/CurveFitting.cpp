456a938b37a76bd25c12252d92b2924b2c2605c7
Pankaj Jadhav <pankajjadhav1712@gmail.com>
2026-06-01 23:08:55 +0530

    Clean up comments in CurveFitting.cpp
    Removed comments related to matrix resizing and polynomial fitting details.

--- NC/src/CurveFitting.cpp
+++ NC/src/CurveFitting.cpp
@@ -16,7 +16,6 @@ void CurveFitting::readDataFromFile(const string& filename) {
     int n;
     fin >> n;
 
-    // Resize inherited Matrix to n×2
     rows = n;
     cols = 2;
     data.assign(n, vector<double>(2, 0.0));
@@ -30,9 +29,6 @@ double CurveFitting::xAt(int i) const { return (*this)(i, 0); }
 double CurveFitting::fAt(int i) const { return (*this)(i, 1); }
 int CurveFitting::getN() const { return getRows(); }
 
-// Polynomial curve fitting of given degree using normal equations.
-// Fits: f(x) = c0 + c1*x + c2*x^2 + ... + c_d*x^d
-// Forms (X^T X) c = X^T f and solves it.
 vector<double> CurveFitting::fit(int degree) const {
     int n = getN();
     int m = degree + 1; // number of coefficients
