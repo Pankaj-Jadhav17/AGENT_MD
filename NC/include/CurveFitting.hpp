e0fe44ce3de1e421bb274e8240c16a06814588d7
Pankaj-Jadhav17 <pankajjadhav1712@gmail.com>
2026-06-09 22:08:36 +0530

    update under NC file

--- NC/include/CurveFitting.hpp
+++ NC/include/CurveFitting.hpp
@@ -4,9 +4,6 @@
 #include "Matrix.hpp"
 #include <vector>
 
-// CurveFitting inherits Matrix.
-// Stores data points as an n×2 matrix (col-0 = x, col-1 = f(x)).
-// Provides polynomial curve fitting via normal equations.
 class CurveFitting : public Matrix {
 public:
     CurveFitting();
@@ -24,10 +21,8 @@ public:
     // Polynomial fit of given degree: f(x) = c0 + c1*x + c2*x^2 + ...
     vector<double> fit(int degree) const;
 
-    // Evaluate polynomial at xval given coefficients
     double evaluate(double xval, const vector<double>& coeffs) const;
 
-    // Root mean square error
     double rmsError(const vector<double>& coeffs) const;
 
     // Display
@@ -35,4 +30,4 @@ public:
     void displayFit(int degree, ostream& os = cout) const;
 };
 
-#endif // CURVEFITTING_HPP
+#endif
